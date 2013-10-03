dfc2fd37940fc836bc2a4a95dc99d173e527da7e
Silvia Rebelo <silvia.rebelo@springstudios.com>
2013-10-03 10:05:24 +0100

    Committing a change to test GUI

--- index.php
+++ index.php
@@ -141,6 +141,7 @@
 			<input type="hidden" name="photo-filepath" id="photo-filepath" />
 			<input type="submit" id="save" value="Save entry" />
 		</form>
+		This is a test...
 	</div>
 </body>
 </html>
